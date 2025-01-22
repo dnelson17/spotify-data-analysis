ef4a4f975483f3fbdab981f81f9e45394b6afbe1
David Nelson <davidnelson99@outlook.com>
2025-01-22 12:24:57 +1100

    Add __init__.py to expose StreamingHistory and StreamingHistoryAnalyser

---
+++ spotify_analysis/__init__.py
@@ -0,0 +1,8 @@
+
+from spotify_analysis.src.data.streaming_history import StreamingHistory
+from spotify_analysis.src.analysis.streaming_history_analyser import StreamingHistoryAnalyser
+
+__all__ = [
+    "StreamingHistory",
+    "StreamingHistoryAnalyser",
+]
